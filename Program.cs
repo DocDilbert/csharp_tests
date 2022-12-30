6bb565e84e5faf06ea00bba005009f7bba8e5d6a
Christian Gröling <ch.groeling@gmail.com>
2022-12-30 12:41:10 +0100

    add nlog

--- Program.cs
+++ Program.cs
@@ -1,7 +1,38 @@
-﻿internal class Program
+﻿using System;
+
+class A
 {
+  public
+  A() {
+
+  }
+}
+
+interface IB {
+  void execute();
+}
+class B : IB{
+  public B(A b) {
+
+  }
+
+  public void execute()
+  {
+    throw new NotImplementedException();
+  }
+}
+
+internal class Program
+{
+    private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+
     private static void Main(string[] args)
     {
+      Logger.Info("Hello wojjjrld");
         Console.WriteLine("Hello, World!");
+        var blabla = new A();
+        var B = new B(blabla);
+        B b;
+
     }
 }
